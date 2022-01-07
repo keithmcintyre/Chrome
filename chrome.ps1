3c51720f4eda6d33e6d67b41acb9f2f946b01732
keithmcintyre <46344399+keithmcintyre@users.noreply.github.com>
2022-01-07 15:25:57 +0000

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -1,6 +1,6 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 $url1 = "http://172.17.0.21:8080"
-$url2 = "https://www.salesforce.com/"
+$url2 = "https://coronavirus.data.gov.uk/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
 Start-Process -FilePath $pathToChrome -ArgumentList $url2, --no-sandbox
